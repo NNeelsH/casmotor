4b33a80f5db55581615e271681bc1bbdb4ba3b33
NNeelsH <heathcliff.neels@gmail.com>
2021-03-14 17:28:15 +1300

    Quiz Week 3 Qs 12 and 13

--- 2_analytics/CAS.R
+++ 2_analytics/CAS.R
@@ -1,1 +1,7 @@
 cas_ds <- read.csv("1_data/freMTPL2freq.csv")
+head(cas_ds)
+table(cas_ds$VehBrand)
+#week 3 quiz 12
+prop.table(table(cas_ds$VehBrand))
+#week 3 quiz 13
+prop.table(table(cas_ds$Area))
